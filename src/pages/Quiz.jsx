26623a8f4ea1fe681d333c6ac65da20baef6bc72
rimpleshrestha <rimpleshrestha133@gmail.com>
2026-01-28 19:56:21 +0545

    ui fixes

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -139,7 +139,7 @@ const Quiz = () => {
     setCurrentQuestionIndex((prev) => prev - 1);
   };
 
-  // Result View (With Logo)
+  // Result View 
   if (skinType) {
     const description = skinTypeDescriptions[skinType];
     return (
